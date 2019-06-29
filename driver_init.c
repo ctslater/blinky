f14f735fe89f8b8c168bd6ddc797c09eb1631db2
Colin Slater <ctslater@uw.edu>
2019-06-29 15:13:36 -0700

    Initial export from Atmel Start.

---
+++ driver_init.c
@@ -0,0 +1,42 @@
+/*
+ * Code generated from Atmel Start.
+ *
+ * This file will be overwritten when reconfiguring your Atmel Start project.
+ * Please copy examples or other code you want to keep to a separate file
+ * to avoid losing it when reconfiguring.
+ */
+
+#include "driver_init.h"
+#include <peripheral_clk_config.h>
+#include <utils.h>
+#include <hal_init.h>
+
+void delay_driver_init(void)
+{
+	delay_init(SysTick);
+}
+
+void system_init(void)
+{
+	init_mcu();
+
+	// GPIO on PA22
+
+	gpio_set_pin_level(D13,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   false);
+
+	// Set pin direction to output
+	gpio_set_pin_direction(D13, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_function(D13, GPIO_PIN_FUNCTION_OFF);
+
+	// GPIO on PA23
+
+	gpio_set_pin_function(D12, GPIO_PIN_FUNCTION_OFF);
+
+	delay_driver_init();
+}

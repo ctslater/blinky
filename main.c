9711f7f441eb1e3603df289219f74245eb071dc1
Colin Slater <ctslater@uw.edu>
2019-07-01 18:53:37 -0700

    Enable USB serial output.

--- main.c
+++ main.c
@@ -1,14 +1,21 @@
 #include <atmel_start.h>
+#include "usb_start.h"
 
 int main(void)
 {
     
+    char buf[] = "AB";
+    int ret;
     /* Initializes MCU, drivers and middleware */
     atmel_start_init();
 
-    /* Replace with your application code */
+    composite_device_start();
+
     while (1) {
         gpio_set_pin_level(D13, false);
+        if (cdcdf_acm_is_enabled()) {
+            ret = cdcdf_acm_write(buf, 2);
+        }
         delay_ms(1000);
         gpio_set_pin_level(D13, true);
         delay_ms(1000);
